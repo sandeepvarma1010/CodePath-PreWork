ded47aeb50bf998e8d3b425cee048e78a9dbb752
sandeepvarma1010 <103293241+sandeepvarma1010@users.noreply.github.com>
2024-02-02 16:35:01 -0800

    Updated View Controller

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -8,7 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var Colorlabel: UILabel!
+    
+    @IBAction func Changelabelcolor(_ sender: UIButton) {
+        let randomColor = getRandomColor()
+            Colorlabel.textColor = randomColor
+        }
 
+        func getRandomColor() -> UIColor {
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+        }
     @IBAction func changeBackgroundColor(_ sender: UIButton) {
         let randomColor = changeColor()
            view.backgroundColor = randomColor
